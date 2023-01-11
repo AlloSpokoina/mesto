fcdb85b6aec0c918d439cd85c703cbfaed1c77e8
Valeriya Markina <118906987+AlloSpokoina@users.noreply.github.com>
2023-01-11 18:59:54 +0500

    add edit form js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let popupOpen = document.querySelector(".popup");
 let openPopupButtons = document.querySelector(".profile__edit-button");
 let closePopupButtons = document.querySelector('.popup__close');
-let form = document.querySelector(".popup__container");
+
 
 openPopupButtons.addEventListener("click", openPopup);
 closePopupButtons.addEventListener("click", closePopup);
@@ -13,3 +13,24 @@ function closePopup() {
 function openPopup() {
   popupOpen.classList.add("active");
 }
+
+let editForm = document.querySelector(".popup__form");
+let profileName = document.querySelector(".profile__name");
+let profileDescription = document.querySelector(".profile__description");
+let profileEditName = document.querySelector(".popup__name");
+let profileEditInfo = document.querySelector(".popup__info");
+
+function handleProfileEditButton () {
+  openPopup(popupOpen);
+  profileEditName.value = profileName.textContent;
+  profileEditInfo.value = profileDescription.textContent;
+};
+
+function retrieveFormValue(event) {
+  event.preventDefault();
+  profileName.textContent = profileEditName.value;
+  profileDescription.textContent = profileEditInfo.value;
+  closePopup(closePopupButtons);
+}
+openPopupButtons.addEventListener('click', handleProfileEditButton);
+editForm.addEventListener("submit", retrieveFormValue);
